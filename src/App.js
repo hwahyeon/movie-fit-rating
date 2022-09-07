1921a28c82fe64b449b9537cc3742429d40b0d5e
hwahyeon <hwahyeon@knou.ac.kr>
2022-09-08 02:13:50 +0900

    Moved to another repo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
     return (
         <Router>
-            <Routes>
+            <Switch>
+                <Route path="/hello">
+                    <h1>Hello</h1>
+                </Route>
+                <Route path="/movie/:id">
+                    <Detail />
+                </Route>
                 <Route path="/">
                     <Home />
                 </Route>
-                <Route path="/movie" element={<Detail />} />
-            </Routes>
+            </Switch>
         </Router>
     );
 }
