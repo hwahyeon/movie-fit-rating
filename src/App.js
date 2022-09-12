c3aec9b51ea136ef06e84a031ce90ff494aac3ed
hwahyeon <hwahyeon@knou.ac.kr>
2022-09-12 23:47:31 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-
+import Home from "./routes/Home";
 function App() {
     return (
         <Router>
             <Switch>
-                <Route path="/hello" basename={process.env.PUBLIC_URL}>
-                    <h1>Hello</h1>
-                </Route>
-                <Route path="/movie/:id" basename={process.env.PUBLIC_URL}>
+                <Route path="/movie/:id">
                     <Detail />
                 </Route>
-                <Route path="/" basename={process.env.PUBLIC_URL}>
+                <Route path="/">
                     <Home />
                 </Route>
             </Switch>
