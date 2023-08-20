3f2ff68c54046b88faa3d3e54c7a2e8328f75d02
hwahyeon <hwahyeon@knou.ac.kr>
2023-08-20 20:43:31 +0900

    Add a 'NO IMAGE AVAILABLE' sign
    이미지 없을 경우, 대체 이미지 출력

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styles from "./Movie.module.css";
+import noImage from "../assets/noimage.png";
 
 function Movie({ id, coverImg, title, year, summary, genres }) {
+    const [currentImg, setCurrentImg] = useState(coverImg);
+
     return (
         <div className={styles.movie}>
-            <img src={coverImg} alt={title} className={styles.movie__img} />
+            <img 
+                src={currentImg} 
+                alt={title} 
+                className={styles.movie__img} 
+                onError={() => setCurrentImg(noImage)}
+            />
             <h2 className={styles.movie__title}>
                 <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>
                     {title}
@@ -19,7 +28,7 @@ function Movie({ id, coverImg, title, year, summary, genres }) {
                 {genres.map((g, index) => (
                     <span key={g}>
                         {g}
-                        {index !== genres.length - 1 && '|'}
+                        {index !== genres.length - 1 && ' | '}
                     </span>
                 ))}
             </ul>
